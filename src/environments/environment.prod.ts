45ac5400dad6bc476508eaafa097d4d44f3c6df2
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-15 23:35:27 +0200

    feat: add environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+/**
+ * Environment settings for production environment
+ */
 export const environment = {
-  production: true
+  production: true,
+  APP_NAME: 'Sciddle',
+  DATABASE_ENTITIES: 'sciddle',
+  DATABASE_SETTINGS: 'sciddle_settings',
+
+  NAME: require('../../package.json').name,
+  VERSION: require('../../package.json').version,
+  LICENSE: require('../../package.json').license,
+  HOMEPAGE: require('../../package.json').homepage,
+  TAGS: require('../../tags.json').tags,
 };
