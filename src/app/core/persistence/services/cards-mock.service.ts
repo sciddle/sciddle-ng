bcb5660d3fa3230d781134e7e855e19d1442ef0f
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-16 21:44:51 +0200

    fix: fade card on swipe out

--- src/app/core/persistence/services/cards-mock.service.ts
+++ src/app/core/persistence/services/cards-mock.service.ts
@@ -8,7 +8,7 @@ export class CardsMockService {
 
   public CARDS: Card[] = [
     new Card('Vegan', ['Essen', 'Ernährung', 'Fleisch', 'Vegetarisch', 'Milch'], 1),
-    // new Card('Vegetarisch', ['Fleisch', 'Essen', 'Vegan', 'Ernährung', 'Tiere'], 1)
+    new Card('Vegetarisch', ['Fleisch', 'Essen', 'Vegan', 'Ernährung', 'Tiere'], 1)
   ];
 
   constructor() {
