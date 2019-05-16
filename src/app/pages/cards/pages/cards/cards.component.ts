f7151f37aec0ee62da4c1338dc7982376847d4d8
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-16 02:08:35 +0200

    feat: display single card #6

--- src/app/pages/cards/pages/cards/cards.component.ts
+++ src/app/pages/cards/pages/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {Media} from '../../../../core/ui/model/media.enum';
 import {environment} from '../../../../../environments/environment';
 import {MatDialog, MatIconRegistry} from '@angular/material';
@@ -8,16 +8,22 @@ import {takeUntil} from 'rxjs/operators';
 import {Subject} from 'rxjs';
 import {MaterialIconService} from '../../../../core/ui/services/material-icon.service';
 import {DomSanitizer} from '@angular/platform-browser';
+import {Direction, StackConfig, SwingCardComponent, SwingStackComponent} from 'angular2-swing';
+import {Card} from '../../../../core/entity/model/card/card.model';
+import {CardsMockService} from '../../../../core/persistence/services/cards-mock.service';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent implements OnInit, OnDestroy {
+export class CardsComponent implements OnInit, AfterViewInit, OnDestroy {
 
   /** App title */
   public title = environment.APP_NAME;
+
+  /** Array of cards */
+  public cards: Card[] = [];
 
   /** Title color */
   public titleColor = 'black';
@@ -29,8 +35,15 @@ export class CardsComponent implements OnInit, OnDestroy {
   /** Helper subject used to finish other subscriptions */
   private unsubscribeSubject = new Subject();
 
+  @ViewChild('myswing1') swingStack: SwingStackComponent;
+  @ViewChildren('mycards1') swingCards: QueryList<SwingCardComponent>;
+
+  stackConfig: StackConfig;
+  recentCard = '';
+
   /**
    * Constructor
+   * @param cardsMockService cards mock service
    * @param dialog dialog
    * @param iconRegistry iconRegistry
    * @param mediaService media service
@@ -38,7 +51,8 @@ export class CardsComponent implements OnInit, OnDestroy {
    * @param materialIconService material icon service
    * @param sanitizer sanitizer
    */
-  constructor(public dialog: MatDialog,
+  constructor(private cardsMockService: CardsMockService,
+              public dialog: MatDialog,
               private iconRegistry: MatIconRegistry,
               private mediaService: MediaService,
               private materialColorService: MaterialColorService,
@@ -57,8 +71,21 @@ export class CardsComponent implements OnInit, OnDestroy {
     this.initializeColors();
     this.initializeMaterial();
     this.initializeMediaSubscription();
+
+    this.initializeStackConfig();
+
+    this.initializeMockCards();
   }
 
+  /**
+   * Handles after-view-init lifecycle phase
+   */
+  ngAfterViewInit() {
+    // Either subscribe in controller or set in HTML
+    this.swingStack.throwin.subscribe((event: DragEvent) => {
+      // event.target.style.background = '#ffffff';
+    });
+  }
 
   /**
    * Handles on-destroy lifecycle phase
@@ -100,6 +127,25 @@ export class CardsComponent implements OnInit, OnDestroy {
     });
   }
 
+  private initializeStackConfig() {
+    this.stackConfig = {
+      allowedDirections: [Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT],
+      throwOutConfidence: (offsetX, offsetY, element) => {
+        return Math.min(Math.abs(offsetX) / (element.offsetWidth / 2), 1);
+      },
+      transform: (element, x, y, r) => {
+        this.onItemMove(element, x, y, r);
+      },
+      throwOutDistance: (d) => {
+        return 800;
+      }
+    };
+  }
+
+  private initializeMockCards() {
+    this.cards = this.cardsMockService.CARDS;
+  }
+
   //
   // Actions
   //
@@ -109,5 +155,37 @@ export class CardsComponent implements OnInit, OnDestroy {
    * @param menuItem menu item that has been clicked
    */
   onMenuItemClicked(menuItem: string) {
+  }
+
+  onItemMove(element, x, y, r) {
+    let color = '';
+    const abs = Math.abs(x);
+
+    const min = Math.trunc(Math.min(16 * 16 - abs, 16 * 16));
+    const hexCode = this.decimalToHex(min, 2);
+
+    if (x < 0) {
+      color = '#FF' + hexCode + hexCode;
+    } else {
+      color = '#' + hexCode + 'FF' + hexCode;
+    }
+
+    element.style.background = color;
+    element.style['transform'] = `translate3d(0, 0, 0) translate(${x}px, ${y}px) rotate(${r}deg)`;
+  }
+
+  //
+  // Helpers
+  //
+
+  private decimalToHex(d, padding) {
+    let hex = Number(d).toString(16);
+    padding = typeof (padding) === 'undefined' || padding === null ? padding = 2 : padding;
+
+    while (hex.length < padding) {
+      hex = '0' + hex;
+    }
+
+    return hex;
   }
 }
