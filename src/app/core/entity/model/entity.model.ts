8d7a9c9d1e57fd83fb6eacdb32d0d01ac93ac892
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-17 23:02:14 +0200

    feat: add some more cards #6

--- src/app/core/entity/model/entity.model.ts
+++ src/app/core/entity/model/entity.model.ts
@@ -10,17 +10,11 @@ export abstract class Entity {
   entityType: EntityType;
   /** Unique identifier */
   id: string;
-  /** Creation date */
-  creationDate: Date;
-  /** Modification date */
-  modificationDate: Date;
 
   /**
    * Constructor
    */
   protected constructor() {
     this.id = new UUID().toString();
-    this.creationDate = new Date();
-    this.modificationDate = new Date();
   }
 }
