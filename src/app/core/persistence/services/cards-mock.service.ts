b30d3b6c2dbfe6b7c2fcc395e742f4e22b49eb72
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-17 23:44:21 +0200

    Fixed some lint issues

--- src/app/core/persistence/services/cards-mock.service.ts
+++ src/app/core/persistence/services/cards-mock.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
 import mockCards from '../../../../assets/cards/cards.json';
 import {Card} from '../../entity/model/card/card.model';
 
