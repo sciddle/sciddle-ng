eae2e04c8bcb782127c29dd12d2052dd40916761
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-21 01:24:41 +0200

    feat: add routing

--- src/app/pages/cards/cards-routing.module.ts
+++ src/app/pages/cards/cards-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CardsComponent} from './pages/cards/cards.component';
+import {StackResolver} from './resolvers/stack.resolver';
 
 const routes: Routes = [
   {path: '', component: CardsComponent},
+  {path: 'cards/:uid', component: CardsComponent, resolve: {stack: StackResolver}},
 ];
 
 @NgModule({
