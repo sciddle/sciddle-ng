4e01c0365bd0b221e547db40c79c44763b4d5851
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-21 00:27:46 +0200

    feat: add methods that merges cards from assets

--- src/app/core/entity/entity.module.ts
+++ src/app/core/entity/entity.module.ts
@@ -1,5 +1,13 @@
-import {NgModule} from '@angular/core';
+import {InjectionToken, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {StacksPersistenceService} from './services/stack/persistence/stacks-persistence.interface';
+import {CardsService} from './services/card/cards.service';
+import {CardsAssetsService} from './services/card/cards-assets.service';
+import {StacksService} from './services/stack/stacks.service';
+import {StacksPouchdbService} from './services/stack/persistence/stacks-pouchdb.service';
+
+/** Injection token for stack persistence PouchDB */
+export let STACK_PERSISTENCE_POUCHDB = new InjectionToken<StacksPersistenceService>('app.stack-persistence-pouchdb');
 
 @NgModule({
   imports: [
@@ -7,6 +15,10 @@ import {CommonModule} from '@angular/common';
   ],
   declarations: [],
   providers: [
+    CardsService,
+    CardsAssetsService,
+    StacksService,
+    {provide: STACK_PERSISTENCE_POUCHDB, useClass: StacksPouchdbService},
   ]
 })
 /**
--- src/app/core/entity/services/card/cards.service.ts
+++ src/app/core/entity/services/card/cards.service.ts
@@ -1,15 +1,35 @@
 import {Injectable} from '@angular/core';
 import {Stack} from '../../model/stack/stack.model';
 import {Card} from '../../model/card/card.model';
+import {Subject} from 'rxjs';
+import {CloneService} from '../clone.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CardsService {
 
+  /** Map of all cards */
+  cards = new Map<string, Card>();
+  /** Subject that publishes cards */
+  cardsSubject = new Subject<Card[]>();
+
   //
   // Sort
   //
+
+  /**
+   * Sorts cards based on their modification date
+   * @param cardA first card
+   * @param cardB seconds card
+   */
+  static sortCards(cardA: Card, cardB: Card) {
+    if (!isNaN(cardA.index) && !isNaN(cardB.index)) {
+      return (cardA.index > cardB.index) ? -1 : 1;
+    } else {
+      return 0;
+    }
+  }
 
   /**
    * Shuffles cards
@@ -30,6 +50,70 @@ export class CardsService {
     }
 
     return cards;
+  }
+
+  //
+  // Initialization
+  //
+
+  /**
+   * Initializes cards
+   * @param cards cards
+   */
+  public initializeCards(cards: Card[]) {
+    this.clearCards();
+    cards.forEach(card => {
+      this.cards.set(card.id, card);
+    });
+    this.notify();
+  }
+
+  /**
+   * Takes cards from assets and merges them into existing stack
+   * @param cardsFromAssets cards loaded from assets
+   */
+  public mergeCardsFromAssets(cardsFromAssets: Card[]): Promise<Card[]> {
+    // Save cards before merge
+    const cardsBefore = CloneService.cloneCards(Array.from(this.cards.values()));
+
+    return new Promise((resolve, dismiss) => {
+      cardsFromAssets.forEach(card => {
+        // Get existing card
+        const existingCard = this.cards.get(card.id);
+
+        if (existingCard != null) {
+          // Update existing card
+          existingCard.word = card.word;
+          existingCard.taboos = card.taboos;
+          existingCard.difficulty = card.difficulty;
+
+          // Add updated card to map
+          this.cards.set(existingCard.id, existingCard);
+        } else {
+          // Simply add new card to map
+          this.cards.set(card.id, card);
+        }
+      });
+
+      // Save cards after merge
+      const cardsAfter = CloneService.cloneCards(Array.from(this.cards.values()));
+
+      // Check if cards have been changed
+      if (JSON.stringify(cardsAfter.toString()) != JSON.stringify(cardsBefore.toString())) {
+        resolve(Array.from(this.cards.values()));
+      } else {
+        this.notify();
+        dismiss();
+      }
+    });
+  }
+
+  /**
+   * Clears cards
+   */
+  public clearCards() {
+    this.cards.clear();
+    this.notify();
   }
 
   constructor() {
@@ -54,5 +138,16 @@ export class CardsService {
 
       resolve();
     });
+  }
+
+  //
+  // Notification
+  //
+
+  /**
+   * Informs subscribers that something has changed
+   */
+  public notify() {
+    this.cardsSubject.next(Array.from(this.cards.values()).sort(CardsService.sortCards));
   }
 }
