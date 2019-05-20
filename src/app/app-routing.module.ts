42939ea4fdf4ff3d647fe54078b10f0583c9104a
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-21 01:24:41 +0200

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'cards', pathMatch: 'full'},
+  {path: '', redirectTo: 'cards/0', pathMatch: 'full'},
   {path: 'cards', loadChildren: './pages/cards/cards.module#CardsModule'},
+  {path: 'cards/:id', loadChildren: './pages/cards/cards.module#CardsModule'},
 ];
 
 @NgModule({
