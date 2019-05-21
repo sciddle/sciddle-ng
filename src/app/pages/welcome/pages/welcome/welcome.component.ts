7d93e702e7f26b3a220455fee7542c9220c6e155
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-21 20:12:02 +0200

    Added navigation to single-player mode

--- src/app/pages/welcome/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/pages/welcome/welcome.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {SnackbarService} from '../../../../core/ui/services/snackbar.service';
 
+/**
+ * Displays welcome page
+ */
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -7,9 +12,34 @@ import {Component, OnInit} from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() {
+  /**
+   * Constructor
+   * @param router router
+   * @param snackbarService snackbar service
+   */
+  constructor(private router: Router,
+              private snackbarService: SnackbarService) {
   }
 
   ngOnInit() {
+  }
+
+  //
+  // Actions
+  //
+
+  /**
+   * Handles click on single-user button
+   */
+  onSinglePlayerClicked() {
+    this.router.navigate([`/cards/0`]).then(() => {
+    });
+  }
+
+  /**
+   * Handles click on multiple-user button
+   */
+  onMultiplePlayerClicked() {
+
   }
 }
