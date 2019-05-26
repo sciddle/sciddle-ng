c586918463e258b80586951e3d19b58f1bce82a1
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-27 00:37:44 +0200

    feat: add automatic shuffling on game start

--- src/app/core/entity/services/game/games.service.ts
+++ src/app/core/entity/services/game/games.service.ts
@@ -1,9 +1,11 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {Stack} from '../../model/stack/stack.model';
 import {CardsService} from '../card/cards.service';
 import {Game} from '../../model/game/game.model';
 import {Team} from '../../model/game/team.model';
 import {ColorService} from '../../../ui/services/color.service';
+import {STACK_PERSISTENCE_POUCHDB} from '../../entity.module';
+import {StacksPersistenceService} from '../stack/persistence/stacks-persistence.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -14,9 +16,11 @@ export class GamesService {
    * Constructor
    * @param cardsService cards service
    * @param colorService color service
+   * @param stacksPersistenceService stacks persistence service
    */
   constructor(private cardsService: CardsService,
-              private colorService: ColorService) {
+              private colorService: ColorService,
+              @Inject(STACK_PERSISTENCE_POUCHDB) private stacksPersistenceService: StacksPersistenceService) {
   }
 
   //
@@ -29,7 +33,6 @@ export class GamesService {
    */
   initializeSinglePlayerGame(stack: Stack): Promise<any> {
     return new Promise((resolve) => {
-      this.cardsService.shuffleStack(stack);
       stack.game = new Game();
       resolve();
     });
@@ -43,7 +46,6 @@ export class GamesService {
    */
   initializeMultiPlayerGame(stack: Stack, teamCount: number): Promise<any> {
     return new Promise((resolve) => {
-      this.cardsService.shuffleStack(stack);
       stack.game = new Game();
       stack.game.teams = [];
 
