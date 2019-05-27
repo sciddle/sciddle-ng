2a4df253cfbc609066250d6e47d2aef5137048d4
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-05-27 19:11:00 +0200

    feat: add confirmation dialog for quitting multiplayer games

---
+++ src/app/ui/confirmation-dialog/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,71 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+/**
+ * Displays confirmation dialog
+ */
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.scss'],
+})
+export class ConfirmationDialogComponent implements OnInit {
+
+  /** Dialog title */
+  dialogTitle = '';
+
+  /** Text to be displayed */
+  text = '';
+  /** Action */
+  action = '';
+  /** Negative action */
+  negativeAction = '';
+  /** Value to be returned */
+  value: any;
+
+  /**
+   * Constructor
+   * @param dialogRef dialog reference
+   * @param data dialog data
+   */
+  constructor(public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles on-init lifecycle phase
+   */
+  ngOnInit() {
+    this.initializeData();
+  }
+
+  //
+  // Initialization
+  //
+
+  /**
+   * Initializes data
+   */
+  private initializeData() {
+    this.dialogTitle = this.data.title;
+    this.text = this.data.text;
+    this.action = this.data.action;
+    this.negativeAction = this.data.negativeAction;
+    this.value = this.data.value;
+  }
+
+  //
+  // Button actions
+  //
+
+  /**
+   * Handles click on confirm button
+   */
+  confirm() {
+    this.dialogRef.close(this.value);
+  }
+}
