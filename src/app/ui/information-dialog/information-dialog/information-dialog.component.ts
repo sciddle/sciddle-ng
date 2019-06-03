5519a4f681aad424b4dee33abd33fc00b64ebdc6
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-06-03 23:04:59 +0200

    Added information dialog

---
+++ src/app/ui/information-dialog/information-dialog/information-dialog.component.ts
@@ -0,0 +1,68 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+/**
+ * Displays information dialog
+ */
+@Component({
+  selector: 'app-information-dialog',
+  templateUrl: './information-dialog.component.html',
+  styleUrls: ['./information-dialog.component.scss']
+})
+export class InformationDialogComponent implements OnInit {
+
+  /** Dialog title */
+  dialogTitle = '';
+
+  /** Text to be displayed */
+  text = '';
+  /** Action */
+  action = '';
+  /** Value to be returned */
+  value: any;
+
+  /**
+   * Constructor
+   * @param dialogRef dialog reference
+   * @param data dialog data
+   */
+  constructor(public dialogRef: MatDialogRef<InformationDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles on-init lifecycle phase
+   */
+  ngOnInit() {
+    this.initializeData();
+  }
+
+  //
+  // Initialization
+  //
+
+  /**
+   * Initializes data
+   */
+  private initializeData() {
+    this.dialogTitle = this.data.title;
+    this.text = this.data.text;
+    this.action = this.data.action;
+    this.value = this.data.value;
+  }
+
+  //
+  // Button actions
+  //
+
+  /**
+   * Handles click on confirm button
+   */
+  confirm() {
+    this.dialogRef.close(this.value);
+  }
+}
