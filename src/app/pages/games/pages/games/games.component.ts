159eba7c2e4f0613d483196b64c4c7fcdd472bb3
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-08-07 03:07:46 +0200

    feat: update credits

--- src/app/pages/games/pages/games/games.component.ts
+++ src/app/pages/games/pages/games/games.component.ts
@@ -308,6 +308,7 @@ export class GamesComponent implements OnInit, AfterViewInit, OnDestroy {
             version: environment.VERSION,
             authorCode: environment.AUTHOR_CODE,
             authorContent: environment.AUTHOR_CONTENT,
+            authorScientificSupervision: environment.AUTHOR_SCIENTIFIC_SUPERVISION,
             licenseCode: environment.LICENSE_CODE,
             licenseContent: environment.LICENSE_CONTENT,
             homepage: environment.HOMEPAGE,
