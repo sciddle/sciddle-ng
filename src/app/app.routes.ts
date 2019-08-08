d82f73c7b908253d774b4e2682c3fe71f3bebe0c
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-08-08 11:09:36 +0200

    Fixed missing documentation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
+/** Rute for stack page */
 export const ROUTE_STACKS = 'stacks';
+/** Rute for games page */
 export const ROUTE_GAMES = 'games';
+/** Rute for cards page */
 export const ROUTE_CARDS = 'cards';
