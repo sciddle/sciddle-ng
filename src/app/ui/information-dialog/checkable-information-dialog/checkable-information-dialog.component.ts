c9c6a5f8aa43c9b9945b50756ad99124c32ee77f
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-09-01 17:13:26 +0200

    feat: add checkable information dialog #34

---
+++ src/app/ui/information-dialog/checkable-information-dialog/checkable-information-dialog.component.ts
@@ -0,0 +1,83 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+/**
+ * Displays checkable information dialog
+ */
+@Component({
+  selector: 'app-checkable-information-dialog',
+  templateUrl: './checkable-information-dialog.component.html',
+  styleUrls: ['./checkable-information-dialog.component.scss']
+})
+export class CheckableInformationDialogComponent implements OnInit {
+
+  /** Dialog title */
+  dialogTitle = '';
+
+  /** Text to be displayed */
+  text = '';
+  /** Checkbox value */
+  checkboxValue: false;
+  /** Checkbox text */
+  checkboxText = '';
+  /** Action */
+  action = '';
+
+  /**
+   * Constructor
+   * @param dialogRef dialog reference
+   * @param data dialog data
+   */
+  constructor(public dialogRef: MatDialogRef<CheckableInformationDialogComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles on-init lifecycle phase
+   */
+  ngOnInit() {
+    this.initializeData();
+  }
+
+  //
+  // Initialization
+  //
+
+  /**
+   * Initializes data
+   */
+  private initializeData() {
+    this.dialogTitle = this.data.title;
+    this.text = this.data.text;
+    this.checkboxValue = this.data.checkboxValue;
+    this.checkboxText = this.data.checkboxText;
+    this.action = this.data.action;
+  }
+
+  //
+  // Actions
+  //
+
+  /**
+   * Handles change of checkbox value
+   * @param event selection state
+   */
+  onCheckboxChanged(event: any) {
+    this.checkboxValue = event.checked;
+  }
+
+  //
+  // Button actions
+  //
+
+  /**
+   * Handles click on confirm button
+   */
+  confirm() {
+    this.dialogRef.close({checkboxValue: this.checkboxValue});
+  }
+}
