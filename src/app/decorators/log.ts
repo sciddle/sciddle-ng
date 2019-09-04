f7a5614006e6ea66c70bff7c725345af7fd85a4d
Florian Schwanz <florian.schwanz@interoberlin.de>
2019-09-04 23:42:46 +0200

    feat: add log methods decorator #36

---
+++ src/app/decorators/log.ts
@@ -0,0 +1,12 @@
+export function Log(): MethodDecorator {
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = (...args: any[]) => {
+      console.log(`%c++ ${propertyKey}`, 'color: grey');
+      return originalMethod.apply(this, args);
+    };
+
+    return descriptor;
+  };
+}
